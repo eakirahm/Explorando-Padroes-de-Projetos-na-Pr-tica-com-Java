cb4d25e4cd2f817d26c6f82e5cc28254725c31e5
eakirahm <eakirahm@gmail.com>
2022-01-28 22:40:30 -0300

    first commit

---
+++ src/main/java/one/digitalinnovation/gof/Application.java
@@ -0,0 +1,24 @@
+package one.digitalinnovation.gof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * Projeto Spring Boot gerado via Spring Initializr.
+ * Os seguinte módulos foram selecionados:
+ * - Spring Data JPA
+ * - Spring Web
+ * - H2 Database
+ * - OpenFeingn
+ */
+
+@EnableFeignClients
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
